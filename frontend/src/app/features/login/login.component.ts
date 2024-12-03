28a30af388bb076758006a2a9c77fa2308739f0f
Marek Gálik <galik.marekk@gmail.com>
2024-12-03 13:10:26 +0100

    feat: FE auth and visual

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
-import { ILoginDto, ILoginResponse } from '../../shared/auth/login.interface';
+import { ILoginDto } from '../../shared/auth/login.interface';
 
 @Component({
   selector: 'app-login',
@@ -18,29 +18,21 @@ export class LoginComponent {
 
   constructor(private authService: AuthService, private router: Router) {}
 
+  ngOnInit() {
+    this.authService.isAuthenticated$.subscribe((authStatus) => {
+      if (authStatus) {
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
+
   onSubmit() {
     if (this.username && this.password) {
       const loginDto: ILoginDto = {
         username: this.username,
         password: this.password,
       };
-
-      this.authService.login(loginDto).subscribe({
-        next: (response: ILoginResponse) => {
-          console.log('Login Successful!');
-
-          // Store tokens in localStorage
-          localStorage.setItem('accessToken', response.access);
-          localStorage.setItem('refreshToken', response.refresh);
-
-          // Navigate to the dashboard
-          this.router.navigate(['/dashboard']);
-        },
-        error: (error) => {
-          console.error('Login failed!', error);
-          this.errorMessage = 'Invalid username or password';
-        },
-      });
+      this.authService.login(loginDto)
     } else {
       this.errorMessage = 'Please fill in both username and password!';
     }
