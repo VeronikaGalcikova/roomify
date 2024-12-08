648e98d6c6bda00640cc4c7a8262225152b46619
VeronikaGalcikova <v.galcikova@gmail.com>
2024-12-08 14:26:23 +0100

    feat(room_reader): add endpoint to get paginated and filtered room readers

--- backend/room_reader/views.py
+++ backend/room_reader/views.py
@@ -1,3 +1,4 @@
+from core.utils import validate_pagination_params, filter_and_paginate_queryset
 from core.validators import is_valid_uuid
 from room_reader.models import RoomEntryLog, RoomReader, UserAgreement
 from room_reader.serializers import RoomEntryLogSerializer, RoomReaderSerializer, UserAgreementSerializer
@@ -6,6 +7,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from core.permissions import IsSuperUserOrReadOnly, IsSuperUserOnly
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db.models import Q
 
 
 class RoomReaderViewSet(viewsets.ModelViewSet):
@@ -14,6 +16,48 @@ class RoomReaderViewSet(viewsets.ModelViewSet):
 
     queryset = RoomReader.objects.all()
     serializer_class = RoomReaderSerializer
+
+    @action(detail=False, methods=['post'], url_path='filter')
+    def get_filtered_room_readers(self, request):
+        # Validate pagination parameters
+        pagination = validate_pagination_params(request.data.get('page'), request.data.get('limit'))
+        if isinstance(pagination, Response):
+            # If validation fails, return the Response object
+            return pagination
+
+        # Unpack validated values
+        page = pagination['page']
+        limit = pagination['limit']
+
+        # Build filters based on optional parameters
+        filters = Q()
+        if uid := request.data.get('uid'):
+            filters &= Q(uid__icontains=uid)  # Filter by UID containing the substring
+
+        if name := request.data.get('name'):
+            filters &= Q(name__icontains=name)  # Filter by name containing the substring
+
+        if ip := request.data.get('ip'):
+            filters &= Q(ip__icontains=ip)  # Filter by IP containing the substring
+
+        if active := request.data.get('active'):
+            # Ensure the `active` value is a boolean
+            if isinstance(active, bool):
+                filters &= Q(active=active)
+            else:
+                # Convert string "true"/"false" to boolean
+                if str(active).lower() == 'true':
+                    filters &= Q(active=True)
+                elif str(active).lower() == 'false':
+                    filters &= Q(active=False)
+                else:
+                    return Response(
+                        {"detail": "'active' should be a boolean or 'true'/'false' string."},
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+
+        # Apply filters to the queryset, paginate it and return as serialized response
+        return filter_and_paginate_queryset(self, filters, page, limit)
 
 
 class UserAgreementViewSet(viewsets.ModelViewSet):
