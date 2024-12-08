08b29cfd63a9da3a98a2cc566ddd0a584b468ae5
VeronikaGalcikova <v.galcikova@gmail.com>
2024-12-08 15:11:40 +0100

    style: change line endings of docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,6 +22,20 @@ else:
     print("Superuser already exists.")
 EOF
 
+# Create the room reader superuser if it doesn't exist
+echo "Creating room reader superuser..."
+python /app/manage.py shell <<EOF
+from django.contrib.auth.models import User
+from django.core.management import call_command
+
+# Check if the user already exists
+if not User.objects.filter(username='room_reader_user').exists():
+    User.objects.create_superuser('room_reader_user', 'room_reader_user@mail.com', 'room_reader_password')
+    print("Room reader superuser created!")
+else:
+    print("Room reader superuser already exists.")
+EOF
+
 # Start the Django development server
 echo "Starting Django development server..."
 python manage.py runserver 0.0.0.0:8000
